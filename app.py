03217c9d9b717d3d77d27746dcd6f363494384e8
Kartik Yendamuri <kartikyendamuri@gmail.com>
2025-11-23 13:41:26 -0500

    step 4

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ number = input("Enter a number: ")
 # Convert to float and perform a calculation
 value = float(number)
 result = value * 5   # simple example: multiply input by 5
+
+# Display result
+print(f"Your number multiplied by 5 is: {result}")
