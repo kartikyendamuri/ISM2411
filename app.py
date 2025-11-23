0dd17ca7c8c65ef2c4d130352b4db2b7e1f0859d
Kartik Yendamuri <kartikyendamuri@gmail.com>
2025-11-23 13:40:54 -0500

    Step 3

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ print("Welcome to my simple calculator!")
 
 # Ask the user for a number
 number = input("Enter a number: ")
+
+# Convert to float and perform a calculation
+value = float(number)
+result = value * 5   # simple example: multiply input by 5
